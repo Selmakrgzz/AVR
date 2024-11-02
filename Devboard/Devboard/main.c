820d2986b784138335c7d70473029194143c6a1f
Selma Karagøz <selma.krgz@outlook.com>
2024-11-02 15:49:16 +0100

    The code runs

--- Devboard/Devboard/main.c
+++ Devboard/Devboard/main.c
@@ -9,10 +9,18 @@
 
 int main(void)
 {
-	PORTA.DIRCLR = PIN6_bm; // Initialiserer PA6 som input
+	PORTA.DIRCLR = PIN6_bm; // Set's PA6 as input - Button pressed down
+	PORTA.DIRSET = PIN5_bm; // Set's PA5 as output - The LED should light up
+	PORTA.PIN6CTRL = PORT_PULLUPEN_bm; // Need a defined condition when it's not activated
 	
     while(1)
     {
         //TODO:: Please write your application code 
+		if (!(PORTA.IN & PIN6_bm)) {
+			PORTA.OUTSET = PIN5_bm;
+		} else {
+			PORTA.OUTCLR = PIN5_bm;
+		}
+		
     }
 }
